98c55c8d55095f4914fe595d35bdb34406c4ff40
NazrinDunya <cefer.eliyev.2000@mail.ru>
2023-10-06 00:36:57 +0400

    day2

---
+++ projects/list.dart
@@ -0,0 +1,31 @@
+void main() {
+  List<dynamic> ededler = [11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 100];
+  print(ededler[0]);
+  print(ededler.elementAt(0));
+  print(ededler.first);
+
+  // ededler.clear();
+  // print(ededler);
+
+  print(ededler.firstWhere((element) => element.isEven));
+
+  // ededler.removeWhere((element) => element.isOdd);
+  // print(ededler);
+
+  // ededler.forEach(print);
+
+  // ededler.add(10);
+  // print(ededler);
+
+  // ededler.insert(0, 55);
+  // print(ededler);
+
+  // ededler.insertAll(0, [1, 2, 3]);
+  // print(ededler);
+
+  List<String> saitler = ['a', 'b', 'c', 'd'];
+  ededler.addAll([...saitler]);
+  print(ededler);
+
+  // List<int> list = [];
+}
---
+++ projects/types.dart
@@ -0,0 +1,70 @@
+void main() {
+  // int a = 5;
+  // double b = 10.6;
+  bool c = true;
+  // final res = a.toString();
+  // final res = b.toString();
+  final res = c.toString();
+  print(res.runtimeType);
+
+  // ==============================
+
+  // double a = 12.2;
+  // print(a.toInt());
+
+  // ==============================
+
+  // int a = 32;
+  // print(a.toDouble());
+
+  // ==============================
+
+  // dynamic a = 45;
+  // a = "abc";
+  // a = 56.34;
+  // print(a);
+  // dynamicdə istədiyimiz typeda dəyər verə və sonradan dəyişə bilirik
+
+  // var a = 45;
+  // a = "abc";
+  // print(a);
+  // var isə ancaq bir typeda dəyər alır
+
+  // ================================
+  int first = 10;
+  int second = 20;
+  int third = 30;
+  int biggest;
+  if (first > second && first > third) {
+    biggest = first;
+  } else if (second > first && second > third) {
+    biggest = second;
+  } else {
+    biggest = third;
+  }
+  print(biggest);
+
+  //==================================
+  Months(String days) {
+  switch (days) {
+    case "September":
+      print("30 gün");
+      break;
+    case "October":
+      print("31 gün");
+      break;
+    case "November":
+      print("30 gün");
+      break;
+    case "December":
+      print("31 gün");
+      break;
+    default:
+  }
+}
+
+  Months("December");
+
+  
+}
+
