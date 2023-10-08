c191bfe606dbfdfc9448f0d4abd7ab42d70a9692
NazrinDunya <cefer.eliyev.2000@mail.ru>
2023-10-08 21:55:32 +0400

    day3

--- projects/functions.dart
+++ projects/functions.dart
@@ -1,18 +1,26 @@
 void main() {
   printSurname(surname: "Dunyamaliyeva");
-  List<int> list = [11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
-  printEvenelements(list);
+  final evenlist = printEvenelements([11, 12, 13, 14, 15, 16, 17, 18, 19, 20]);
+  print(evenlist);
 }
 
 void printSurname({required String surname}) {
   print("surname: $surname");
 }
 
-
-printEvenelements(List <int> list) {
+printEvenelements(List<int> list) {
   for (var i = 0; i < list.length; i++) {
-    if (i.isEven) {
-      print(i);
+    if (list[i].isOdd) {
+      list.remove(list[i]);
     }
   }
+  return list;
 }
+// printEvenelements(List<int> list) {
+//   for (var i = 0; i < list.length; i++) {
+//     if (i.isEven) {
+//       print(i);
+//     }
+//   }
+// }
+
